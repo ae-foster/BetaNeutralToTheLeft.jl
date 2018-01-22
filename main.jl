47d942db807eec2dfa5545f39c11e6c05b9de2c7
Adam Foster <a.evan.foster@gmail.com>
2018-01-22 14:29:19 +0000

    Significant speed-up to method 1 approximation; fine tune timings

--- main.jl
+++ main.jl
@@ -1,15 +1,16 @@
 using ProgressMeter
 using Distributions
+include("dirichlet_multinomial.jl")
 
 true_dataset = true
 debug = false
 
 if true_dataset # Data from source
     include("dataset.jl")
-    filename = "/Users/EmileMathieu/code/NTL/reviews.json"
+    filename = "./reviews.json"
     z, X = getDocumentTermMatrixFromReviewsJson(filename)
-    X = X[1:200, :]
-    z = z[1:200]
+    X = X[1:10000, :]
+    z = z[1:10000]
     N, D = size(X)
 
 else # Synthetic data
@@ -36,15 +37,14 @@ log_qz = -Inf*ones(Float64, N, K_max)
 qtheta = zeros(Float64, D, K_max)
 
 # Prior (hyper)parameters
-dir_prior_param = 0.01 * ones(Float64, D)
-a = 0.1 # inter-arrival Geometric success parameter
-a_prime = 1; b_prime = 1; # Beta prior on a
+dir_prior_param = 0.0005 * ones(Float64, D)
+a_prime = 1; b_prime = 1; # Beta prior on geometric parameter
 alpha = 0.5 # Neutral to the left parameter
 
 # Partial sums for qz^pr
 S_n = ones(Float64, K_max) # for efficiently computing E[n_k]
 # Method for estimating qz
-qzn_estimator_method = 3
+qzn_estimator_method = 1
 # Only updated when using qzn_estimator_method == 2
 Sprod = ones(Float64, K_max) # for efficiently computing E[K_{n-1}]
 
@@ -60,14 +60,6 @@ epsilon = 0.1
 p = Progress(N, .5, "Observation n°: ", 50)
 log_qz[1, 1] = 0 # Initialize first data point
 qtheta[:, 1] = dir_prior_param + X[1, :] # Initialize qtheta posterior given x_1
-
-function logp_dirichlet_multinomial(x::Union{Vector,SparseVector}, alpha::Vector)
-    # cf https://en.wikipedia.org/wiki/Dirichlet-multinomial_distribution
-    n_x = sum(x)
-    alpha_0 = sum(alpha)
-    mask = findnz(x)[1]
-    return log(n_x) + lbeta(alpha_0, n_x) - sum(log(x[mask])) + sum(lbeta.(x[mask], alpha[mask]))
-end
 
 function qzn_pr_estimator_1(S_n::Vector, n::Int, K_max::Int, alpha::Float64)
     unnormalized = max.((S_n - alpha),0)
@@ -108,8 +100,11 @@ function print_debug(args...)
 end
 
 q_pr_time = 0
-dir_time = 0
+dir_n_time = 0
+dir_new_time = 0
 new_cluster_time = 0
+norm_time = 0
+s_n_time = 0
 qtheta_time = 0
 
 for n = 2:N
@@ -131,6 +126,7 @@ for n = 2:N
     # end
     tic()
     if qzn_estimator_method == 1
+        a = a_prime / (a_prime + b_prime)
         log_qzn_pr = log(1-a) + qzn_pr_estimator_1(S_n, n, K_max, alpha)
         log_qzn_pr_new = log(a)
     elseif qzn_estimator_method == 3
@@ -149,33 +145,34 @@ for n = 2:N
 
     # Second projection: Use marginalization of conjugate exp fam
     # i.e. substraction in parameter space
-    for k = 1:K_max
-        log_qz[n,k] = log_qzn_pr[k] + logp_dirichlet_multinomial(X[n, :], qtheta[:, k])
-        if isnan(log_qz[n,k])
-            print_debug("That was a NaN")
-            print_debug(X[n, :])
-            print_debug(qtheta[:, k])
-            error()
-        end
+    log_qz[n,:] = log_qzn_pr + logp_dirichlet_multinomial(X[n, :], qtheta)'
+    if any(isnan, log_qz[n,:])
+        print_debug(X[n, :])X[n, :] .* qzn'
+        print_debug(qtheta)
+        error("NaN encountered in logp_dirichlet computation")
     end
+
+    dir_n_time += toq()
+    tic()
+
     log_qzn_new = log_qzn_pr_new + logp_dirichlet_multinomial(X[n, :], dir_prior_param)
+
+    print_debug("log_qz[n,:] ", log_qz[n,:])
+    print_debug("log_qzn_new ", log_qzn_new)
 
     # Multiply this unnormalized distribution by a constant
     offset = max.(log_qz[n, :], log_qzn_new)[1]
     log_qz[n,:] -= offset
     log_qzn_new -= offset
 
-    print_debug("log_qz[n,:] ", log_qz[n,:])
-    print_debug("log_qzn_new", log_qzn_new)
-
-    dir_time += toq()
+    dir_new_time += toq()
     tic()
     log_new_cluster_prob = log_qzn_new - log(exp(log_qzn_new) + sum(exp.(log_qz[n, :])))
 
     # Should create a new cluster ?
     print_debug("log_new_cluster_prob: ", log_new_cluster_prob)
     if log_new_cluster_prob > log(epsilon)
-        println("New cluster")
+        print_debug("New cluster")
         K_max += 1
         qtheta = hcat(qtheta, dir_prior_param)
         log_qz = hcat(log_qz, -Inf*ones(Float64, N, 1))
@@ -183,27 +180,45 @@ for n = 2:N
         if qzn_estimator_method < 3 push!(S_n, 0) end
         if qzn_estimator_method == 2 push!(Sprod, 1) end
         if qzn_estimator_method == 3 nk_stats = hcat(nk_stats, zeros(Float64, M)) end
+        a_prime += 1
+    else
+        b_prime += 1
     end
     new_cluster_time += toq()
     tic()
 
     print_debug("Unnormalized ", log_qz[n, :])
+
     log_qz[n, 1:K_max] -= log(sum(exp.(log_qz[n, 1:K_max]))) # normalization
+    qzn = exp.(log_qz[n, 1:K_max])
+
     print_debug("Normalized ", log_qz[n, :])
 
-    # sufficient stats for expectation of n_k
-    if qzn_estimator_method < 3 S_n += exp.(log_qz[n, 1:K_max]) end
+    norm_time += toq()
+    tic()
 
-    Scdf = 0
-    for k = 1:K_max
-        # Update global parameter approximation qtheta
-        qtheta[:, k] += exp.(log_qz[n, k]) .* X[n, :] # update global parameter
-        if qzn_estimator_method == 2
+    # sufficient stats for expectation of n_k
+    if qzn_estimator_method < 3
+        S_n += qzn
+    end
+
+    s_n_time += toq()
+    tic()
+
+    # qtheta += X[n, :] .* qzn'
+    # BLAS.gemm!('N', 'T', 1.0, X[n, :], qzn, 1.0, qtheta)
+    mask = findnz(X[n, :])[1]
+    qtheta[mask, :] += X[n, mask] .* qzn'
+
+    if qzn_estimator_method == 2
+        Scdf = 0
+        for k = 1:K_max
             # Sufficient stats for expectation of Kn
             Scdf += exp(log_qz[n, k])
             Sprod[k] *= Scdf
         end
     end
+
     qtheta_time += toq()
 
 end
@@ -215,15 +230,20 @@ println("N: ", N)
 println("D: ", D)
 println("K_max: ", K_max)
 println("qz: ", qz[1:10,1:10])
+println("a_prime: ", a_prime, ", b_prime: ", b_prime)
 if !true_dataset println("qz: ", qz) end
 
 println("--------- computation times ----------")
 println("q_pr ", q_pr_time)
-println("dir ", dir_time)
+println("dir_n ", dir_n_time)
+println("dir_new ", dir_new_time)
 println("new cluster ", new_cluster_time)
+println("norm ", norm_time)
+println("s_n ", s_n_time)
 println("qtheta ", qtheta_time)
 
 # Compute metrics of the approximate posterior distribution
 include("metrics.jl")
 true_nb_clusters, approx_nb_cluster = compare_nb_clusters(z, qz)
-compute_clustering_quality(z, qz)
+println("true_no_clusters ", true_nb_clusters)
+println("Cluster quality metric ", compute_clustering_quality(z, qz))
