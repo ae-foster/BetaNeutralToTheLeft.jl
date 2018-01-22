0cfcb00daa6288dc64f5882b91880aeada0eee01
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2018-01-22 14:34:16 +0000

    split estimators in another file

---
+++ estimators.jl
@@ -0,0 +1,31 @@
+function qzn_pr_estimator_1(S_n::Vector, n::Int, K_max::Int, alpha::Float64)
+    unnormalized = max.((S_n - alpha),0)
+    lp = log.(unnormalized ./ sum(unnormalized))
+    return lp
+end
+
+function qzn_pr_estimator_2(S_n::Vector, Sprod::Vector, n::Int, K_max::Int, a::Float64, alpha::Float64)
+    (1 - a) * max((S_n - alpha),0) / (n - 1 - alpha*(K_max - sum(Sprod)))
+end
+
+function qzn_pr_estimator_MC(qz_prev:: Vector, nk_stats::Matrix, T_Kprev::Vector, M::Int, n::Int, K_max::Int, a::Float64, alpha::Float64, a_prime::Float64, b_prime::Float64)
+    # Monte Carlo estimate with M samples
+    # nk_stats Matrix of size MxKmax with n_k
+    # T_Kprev vector of size M with T_K_{n-1}
+
+    qz_prev_samples = wsample(1:K_max, qz_prev, M) # z ~ \hat{q}_{n-1}(.)
+    # update nk
+    for m in 1:M
+        if nk_stats[m, qz_prev_samples[m]] == 0 T_Kprev[m] = n-1 end
+        nk_stats[m, qz_prev_samples[m]] += 1
+    end
+    Kprev = sum(nk_stats .!= 0, 2)
+    qzn_pr = zeros(Float64, K_max + 1)
+    p_new_cluster = (Kprev - 1 + a_prime) ./ (T_Kprev + a_prime + b_prime)
+    qzn_pr[K_max+1] = mean(p_new_cluster)
+    for k in 1:K_max
+        qzn_pr[k] = mean( (1 - p_new_cluster) .* (nk_stats[:,k] - alpha) ./ (n - 1 - alpha.*Kprev) )
+    end
+
+    nk_stats, T_Kprev, log.(qzn_pr[1:K_max]),  log(qzn_pr[K_max+1])
+end
--- main.jl
+++ main.jl
@@ -1,6 +1,7 @@
 using ProgressMeter
 using Distributions
 include("dirichlet_multinomial.jl")
+include("estimators.jl")
 
 true_dataset = true
 debug = false
@@ -61,38 +62,6 @@ p = Progress(N, .5, "Observation n°: ", 50)
 log_qz[1, 1] = 0 # Initialize first data point
 qtheta[:, 1] = dir_prior_param + X[1, :] # Initialize qtheta posterior given x_1
 
-function qzn_pr_estimator_1(S_n::Vector, n::Int, K_max::Int, alpha::Float64)
-    unnormalized = max.((S_n - alpha),0)
-    lp = log.(unnormalized ./ sum(unnormalized))
-    return lp
-end
-
-function qzn_pr_estimator_2(S_n::Vector, Sprod::Vector, n::Int, K_max::Int, a::Float64, alpha::Float64)
-    (1 - a) * max((S_n - alpha),0) / (n - 1 - alpha*(K_max - sum(Sprod)))
-end
-
-function qzn_pr_estimator_MC(qz_prev:: Vector, nk_stats::Matrix, T_Kprev::Vector, M::Int, n::Int, K_max::Int, a::Float64, alpha::Float64)
-    # Monte Carlo estimate with M samples
-    # nk_stats Matrix of size MxKmax with n_k
-    # T_Kprev vector of size M with T_K_{n-1}
-
-    qz_prev_samples = wsample(1:K_max, qz_prev, M) # z ~ \hat{q}_{n-1}(.)
-    # update nk
-    for m in 1:M
-        if nk_stats[m, qz_prev_samples[m]] == 0 T_Kprev[m] = n-1 end
-        nk_stats[m, qz_prev_samples[m]] += 1
-    end
-    Kprev = sum(nk_stats .!= 0, 2)
-    qzn_pr = zeros(Float64, K_max + 1)
-    p_new_cluster = (Kprev - 1 + a_prime) ./ (T_Kprev + a_prime + b_prime)
-    qzn_pr[K_max+1] = mean(p_new_cluster)
-    for k in 1:K_max
-        qzn_pr[k] = mean( (1 - p_new_cluster) .* (nk_stats[:,k] - alpha) ./ (n - 1 - alpha.*Kprev) )
-    end
-
-    nk_stats, T_Kprev, log.(qzn_pr[1:K_max]),  log(qzn_pr[K_max+1])
-end
-
 function print_debug(args...)
     if debug == true
         println(args...)
@@ -130,7 +99,7 @@ for n = 2:N
         log_qzn_pr = log(1-a) + qzn_pr_estimator_1(S_n, n, K_max, alpha)
         log_qzn_pr_new = log(a)
     elseif qzn_estimator_method == 3
-        nk_stats,T_Kprev,log_qzn_pr,log_qzn_pr_new = qzn_pr_estimator_MC(exp.(log_qz[n-1,1:K_max]), nk_stats, T_Kprev, M, n, K_max, a, alpha)
+        nk_stats,T_Kprev,log_qzn_pr,log_qzn_pr_new = qzn_pr_estimator_MC(exp.(log_qz[n-1,1:K_max]), nk_stats, T_Kprev, M, n, K_max, a, alpha, a_prime, b_prime)
     else
         error("Currently unsupported")
     end
