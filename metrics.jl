91aa6cd592fa50c2342d0979bfcacb52d8f1befb
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2018-01-23 12:17:19 +0000

    baseline for coclustering matrix

--- metrics.jl
+++ metrics.jl
@@ -8,12 +8,12 @@ end
 function compute_clustering_quality(z, qz)
     # c_ij = 1 if i and j belongs to same cluster, 0 otherwise
     # c_approx_ij approx probability of belonging to the same cluster
-
+    N = length(z)
     z_hashed = hash.(z)
     c = convert(Matrix{Int32}, z_hashed .== z_hashed')
     c_approx = qz * qz'
-
-   return mean(abs.(c - c_approx))
+    c_approx += - Diagonal(c_approx) + eye(N)
+    return mean(abs.(c - c_approx)), mean(abs.(c - Diagonal(ones(Int, N))))
 end
 
 # predictive log-likelihood ??
