d98d900957efa2dbd3c3f15541e5bc29f72a323b
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2018-01-25 13:28:05 +0000

    predictive log likelihood

--- metrics.jl
+++ metrics.jl
@@ -16,4 +16,12 @@ function compute_clustering_quality(z, qz)
     return mean(abs.(c - c_approx)), mean(abs.(c - Diagonal(ones(Int, N))))
 end
 
-# predictive log-likelihood ??
+function loglikelihood(logp_emission, x, log_qzn, log_qzn_new, qtheta, theta_prior, K_max)
+    log_qx = zeros(Float64, K_max+1)
+    log_qx[K_max+1] = log_qzn_new + logp_emission(x, theta_prior...)
+    log_qx[1:K_max] = log_qzn + reshape(logp_emission(x, qtheta...), K_max)
+
+    offset = max.(log_qx)[1]
+
+    return offset + log(sum(exp.(log_qx)))
+end
