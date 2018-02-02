daeb29d9bd686f84822ee7b0622285536dc8b828
Adam Foster <a.evan.foster@gmail.com>
2018-02-02 16:56:12 +0000

    T plot

---
+++ tplot.jl
@@ -0,0 +1,41 @@
+using Plots
+using StatsBase
+
+f = open("/data/flyrobin/foster/Documents/NTL.jl/sorted-stackoverflow.txt")
+arrival_times = Dict{Any, Int}()
+degrees = Dict{Any, Int}()
+for (i, ln) in enumerate(eachline(f))
+    a = split(ln)
+    start = a[1]
+    terminal = a[2]
+    if ~haskey(arrival_times, start)
+        arrival_times[start] = 2*i - 1
+        degrees[start] = 1
+    else
+        degrees[start] += 1
+    end
+    if ~haskey(arrival_times, terminal)
+        arrival_times[terminal] = 2*i
+        degrees[terminal] = 1
+    else
+        degrees[terminal] += 1
+    end
+end
+
+ts = sort(collect(values(arrival_times)))
+degs = collect(values(degrees))
+
+# Plots.plot(ts)
+# Plots.gui()
+#
+# Plots.histogram(ts[2:end] - ts[1:end-1], bins=100)
+# Plots.gui()
+
+ef = ecdf(degs)
+x = logspace(log(min(degs...)+1), log(max(degs...)-1), 100)
+output = [(1-ef(t)) for t in x]
+Plots.plot(x, output, xscale=:log10, yscale=:log10)
+Plots.gui()
+
+n = sum(degs)
+println("alpha hat ", 1 + n/(sum(log(degs)) - n*log(0.5)))
