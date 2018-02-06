a8fce7bdba3fc81411501b9c257139ca0ec97a0f
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2018-02-06 20:56:27 +0000

    adding comments

--- infer_arrival_times.jl
+++ infer_arrival_times.jl
@@ -2,23 +2,33 @@ using Distributions
 using SpecialFunctions
 
 function logjoint_derivative_Tj(x::Real, j::Int, alpha::Real, p::Distributions.Beta{Float64}, nj_bar::Array{Int})
-    res = digamma(x - j * alpha)
-    res += (j - 2) / (x - 1) - mean(p)
-    res += -digamma(nj_bar[j] - x + 1)
-    if j > 1
-        res += - digamma(x - 1 - (j - 1) * alpha) + digamma(nj_bar[j-1] - x + 2)
-    end
-    return res
+    # Compute the derivative of the log joint with respect to Tj
+
+    grad = digamma(x - j * alpha) # First terms of the product
+    if j > 1 grad += - digamma(x - 1 - (j - 1) * alpha) end
+
+    grad += (j - 2) / (x - 1) - mean(p) # term from the Gamma approximation
+
+    grad += -digamma(nj_bar[j] - x + 1) # terms from combinatorial coefficient
+    if j > 1 grad += digamma(nj_bar[j-1] - x + 2) end
+
+    grad
 end
 
 # function logjoint_derivative_alpha(x::Real, Tj::Array{Real}, n::Int, Kn::Int, nj_bar::Array{Int})
 function logjoint_derivative_alpha(x::Real, Tj, n::Int, Kn::Int, nj_bar::Array{Int})
+    # Compute the derivative of the log joint with respect to alpha
+
+    grad = Kn * digamma(n - Kn * x) # First term of the joint
+
     vec = zeros(Float64, Kn)
     for j in 1:Kn
         vec[j] = -j*digamma(Tj[j] - j * alpha) - digamma(nj[j]) - digamma(nj[j] - x)
         vec[j] += (j-1)*digamma(Tj[j] - 1 - (j - 1) * x) + digamma(1 - x)
     end
-    return Kn * digamma(n - Kn * x) + sum(vec)
+    grad += sum(vec)
+
+    grad
 end
 
 # Load dataset
@@ -33,14 +43,14 @@ ap = 1; bp = 1
 p = Beta(ap + Kn - 1, bp + n - 1)
 
 # Initialise point estimates
-alpha = -2.
+alpha = 0.
 Tj = cumsum(round(n / Kn) * ones(Real, n))
 
 # Optimisation hyperparameters
 nb_epochs = 1
 lr_Tj = .1
 lr_alpha = .01
-println(mean(Tj))
+
 for i in 1:nb_epochs
     println(i)
     for j in 1:Kn
@@ -52,7 +62,6 @@ for i in 1:nb_epochs
         println("Tj[",j,"]=",old," | ",Tj[j])
         # if j > 10 break end
     end
-    println(mean(Tj))
     # step for alpha
     alpha +=-lr_alpha * logjoint_derivative_alpha(alpha, Tj, n, Kn, nj_bar)
 
