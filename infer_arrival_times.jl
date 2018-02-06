fd0ae832d773ff2636db10599a7356afd4a89770
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2018-02-06 22:11:16 +0000

    fix optimisation

--- infer_arrival_times.jl
+++ infer_arrival_times.jl
@@ -1,21 +1,37 @@
 using Distributions
 using SpecialFunctions
 
-function logjoint_derivative_Tj(x::Real, j::Int, alpha::Real, p::Distributions.Beta{Float64}, nj_bar::Array{Int})
+function logjoint_derivative_Tj(x::Real, j::Int, Kn::Int, alpha::Real, p::Distributions.Beta{Float64}, nj_bar::Array{Int})
     # Compute the derivative of the log joint with respect to Tj
 
     grad = digamma(x - j * alpha) # First terms of the product
-    if j > 1
-        grad += - digamma(x - 1 - (j - 1) * alpha)
-        # grad += (j - 2) / (x - 1) - mean(p) # term from the Gamma approximation
-        grad += - mean(p) # term from the Exponential approximation
-    end
+    grad += - digamma(x - 1 - (j - 1) * alpha)
+    # grad += (j - 2) / (x - 1) - mean(p) # term from the Gamma approximation
+    grad += - mean(p) # term from the Exponential approximation
 
     grad += -digamma(nj_bar[j] - x + 1) # terms from combinatorial coefficient
-    if j > 1 grad += digamma(nj_bar[j-1] - x + 2) end
+    grad += digamma(nj_bar[j-1] - x + 2)
+
+    if j == Kn
+        grad += mean(p)
+        grad = min(grad, 0)
+    end # Censoring term for last T_Kn
 
     grad
 end
+
+function logjoint_second_derivative_Tj(x::Real, j::Int, Kn::Int, alpha::Real, nj_bar::Array{Int})
+    # Compute the 2nd derivative of the log joint with respect to Tj
+
+    hess = trigamma(x - j * alpha) # First terms of the product
+    hess += - trigamma(x - 1 - (j - 1) * alpha)
+
+    hess += trigamma(nj_bar[j] - x + 1) # terms from combinatorial coefficient
+    hess += -trigamma(nj_bar[j-1] - x + 2)
+
+    hess
+end
+
 
 # function logjoint_derivative_alpha(x::Real, Tj::Array{Real}, n::Int, Kn::Int, nj::Array{Int})
 function logjoint_derivative_alpha(x::Real, Tj, n::Int, Kn::Int, nj::Array{Int})
@@ -25,8 +41,8 @@ function logjoint_derivative_alpha(x::Real, Tj, n::Int, Kn::Int, nj::Array{Int})
 
     vec = zeros(Float64, Kn)
     for j in 1:Kn
-        vec[j] = -j*digamma(Tj[j] - j * alpha) - digamma(nj[j] - x)
-        if j > 1 vec[j] += (j-1)*digamma(Tj[j] - 1 - (j - 1) * x) + digamma(1 - x) end
+        vec[j] = -j*digamma(Tj[j] - j * x) - digamma(nj[j] - x)
+        if j > 1 vec[j] += (j-1)*digamma(Tj[j] - 1 - (j - 1) * x) end
         vec[j] += digamma(1 - x)
     end
     grad += sum(vec)
@@ -34,11 +50,29 @@ function logjoint_derivative_alpha(x::Real, Tj, n::Int, Kn::Int, nj::Array{Int})
     grad
 end
 
+function logjoint_second_derivative_alpha(x::Real, Tj, n::Int, Kn::Int, nj::Array{Int})
+    # Compute the 2nd derivative of the log joint with respect to alpha
+    hess = - Kn^2 * trigamma(n - Kn * x) # First term of the joint
+
+    vec = zeros(Float64, Kn)
+    for j in 1:Kn
+        vec[j] = j^2*trigamma(Tj[j] - j * x) + trigamma(nj[j] - x)
+        if j > 1 vec[j] += (j-1)^2*trigamma(Tj[j] - 1 - (j - 1) * x) end
+        vec[j] += - trigamma(1 - x)
+    end
+    hess += sum(vec)
+
+    hess
+end
+
+
 # Load dataset
 include("dataset.jl")
 nj, T_data = parseSnapData("data/sorted-sx-mathoverflow.txt")
 Kn = length(nj)
+println("Kn:",Kn)
 n = sum(nj)
+println("n:",n)
 nj_bar = cumsum(nj)
 
 # Posterior distribution of inter-arrival time Geomtric's parameter
@@ -47,28 +81,30 @@ p = Beta(ap + Kn - 1, bp + n - 1)
 
 # Initialise point estimates
 alpha = 0.
-Tj = round.(cumsum((n / Kn) * ones(Real, n)))
+Tj = round.(cumsum((n / Kn) * ones(Real, Kn)))
 
 # Optimisation hyperparameters
-nb_epochs = 1
-lr_Tj = .1
+nb_epochs = 10
+lr_Tj = .01
 lr_alpha = .01
 
 for i in 1:nb_epochs
     println(i)
-    for j in 1:Kn
-        # step for Tj, j=1,...,Kn
-        old = Tj[j]
-        grad = logjoint_derivative_Tj(Tj[j], j, alpha, p, nj_bar)
-        Tj[j] += lr_Tj * logjoint_derivative_Tj(Tj[j], j, alpha, p, nj_bar)
-        if abs(grad) > 1.
-        println("grad:", grad)
-        println("Tj[",j,"]=",old," | ",Tj[j])
-        end
-        # if j > 10 break end
-    end
-    # step for alpha
-    alpha += lr_alpha * logjoint_derivative_alpha(alpha, Tj, n, Kn, nj)
 
-    # Compute & print log joint ?
+    for j in 2:Kn # step for Tj, j=2,...,Kn
+        # old = Tj[j]
+        grad = logjoint_derivative_Tj(Tj[j], j, Kn, alpha, p, nj_bar)
+        second_der = logjoint_second_derivative_Tj(Tj[j], j, Kn, alpha, nj_bar)
+        Tj[j] += lr_Tj * (grad / second_der)
+
+        if Tj[j] < j Tj[j] = j end
+        if Tj[j] > nj_bar[j-1]+1 Tj[j] = nj_bar[j-1]+1 end
+    end
+
+    # step for alpha
+    grad = logjoint_derivative_alpha(alpha, Tj, n, Kn, nj)
+    second_der = logjoint_second_derivative_alpha(alpha, Tj, n, Kn, nj)
+    alpha += lr_alpha * (grad / second_der)
+
+    # TODO: compute & print log joint
 end
