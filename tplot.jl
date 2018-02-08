daae3ddf02abedc07ba19f9135353057b6486f60
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2018-02-08 13:43:44 +0000

    enhance geometric MLE fit cdf plot

--- tplot.jl
+++ tplot.jl
@@ -7,11 +7,13 @@ plotly()
 ## gunzip <name>
 ## sort -k3 -n <name> > sorted-<name>
 
-dir = "/data/flyrobin/foster/Documents/NTL.jl/"
+# dir = "/data/flyrobin/foster/Documents/NTL.jl/"
+dir = "data/"
 for fname in readdir(dir)
     if startswith(fname, "sorted-")
-        println("\n$fname")
         degs, ts = parseSnapData("$dir$fname")
+        fname = split(split(fname, ".txt")[1],"sx-")[end]
+        println("\n$fname")
 
         # Plots.plot(ts, title=fname)
         # Plots.gui()
@@ -30,9 +32,12 @@ for fname in readdir(dir)
         p_hat = length(delta)/sum(delta)
         ef = ecdf(delta)
         ks_stat = max([abs(1 - (1-p_hat)^k - ef(k)) for k=1:max(delta...)]...)
+        efs = [ef(k) for k=1:max(delta...)]
+        max_k = find(efs .< 0.98)[end]
 
-        Plots.plot([ef(k) for k=1:max(delta...)], title=fname)
-        Plots.plot!([1 - (1-p_hat)^k for k=1:max(delta...)])
+        Plots.plot(1:max_k, [ef(k) for k=1:max_k], label = "True data", line=(3))
+        Plots.plot!(1:max_k, [1 - (1-p_hat)^k for k=1:max_k], lw=3, label = "MLE", line=(3,:dash))
+        Plots.plot!(title=fname, xlabel="Inter-arrival time", ylabel="Cumulative distribution function")
         Plots.gui()
 
         println("p_hat ", p_hat)
