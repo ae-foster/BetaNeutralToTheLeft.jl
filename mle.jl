9a102ade0b4bc1f9fe07b74c49429216394d909f
Adam Foster <a.evan.foster@gmail.com>
2018-02-09 11:34:28 +0000

    Fit and record the Frankstein (alpha, theta, tau)-PYP process

--- mle.jl
+++ mle.jl
@@ -21,8 +21,6 @@ for fname in readdir(dir)
         dmap = countmap(degs)
         ds = collect(keys(dmap))
         dcounts = collect(values(dmap))
-        deltas = ts[2:end] - ts[1:(end-1)]
-        lag=Tend-ts[end]
         K = sum(dcounts)
 
         println("\nPYP")
@@ -40,7 +38,7 @@ for fname in readdir(dir)
         results[fname]["PYP"]["ll"] = -result.minimum
         println("Expected powerlaw ", 1+tau)
 
-        println("NTL")
+        println("\nNTL")
         results[fname]["NTL"] = Dict()
         result = optimize(a -> -ntl_llikelihood(a, ds, dcounts, ts, K, Tend),
                           (storage, params) -> neg_grad_ntl_llikelihood!(storage, params, ds, dcounts, ts, K, Tend),
@@ -48,15 +46,31 @@ for fname in readdir(dir)
         alpha = 1 - exp(result.minimizer[1])
         println("NTL alpha ", alpha)
         results[fname]["NTL"]["alpha"] = alpha
-        ll = result.minimum
-        result = optimize(g -> -geom_llikelihood(g, deltas, lag), [.5], LBFGS())
+        ll_ntl = result.minimum
+        result = optimize(g -> -geom_llikelihood(g, K, Tend), [.5], LBFGS())
         g = exp(result.minimizer[1])/(1+exp(result.minimizer[1]))
         println("g ", g)
         results[fname]["NTL"]["g"] = g
-        ll += result.minimum
+        ll = ll_ntl + result.minimum
         println("Optimized ll ", -ll)
         results[fname]["NTL"]["ll"] = -ll
+        results[fname]["NTL"]["ntlll"] = -ll_ntl
         println("Expected powerlaw ", 1+(1/g - alpha)/(1/g - 1))
+
+        println("\nFrankenstein")
+        results[fname]["Frankenstein"] = Dict()
+        result = optimize(params -> -pyp_arr_llikelihood(params, ds, dcounts, ts, K, Tend),
+                          (storage, params) -> neg_grad_pyp_arr_llikelihood!(storage, params, ds, dcounts, ts, K, Tend),
+                          [0., .5], LBFGS())
+        tau = exp(result.minimizer[1])/(1+exp(result.minimizer[1]))
+        theta = result.minimizer[2]
+        ll = ll_ntl + result.minimum
+        results[fname]["Frankenstein"]["tau"] = tau
+        results[fname]["Frankenstein"]["theta"] = theta
+        results[fname]["Frankenstein"]["ll"] = -ll
+        println("tau ", tau, " theta ", theta, " alpha ", alpha)
+        println("Optimized ll ", -ll)
+        println("Expected power law ", 1+tau)
 
         println("\n")
     end
